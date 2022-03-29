ed2c10718fc65682947104af2efe26e429681c60
veeresh soni <veereshsoni913@gmail.com>
2022-03-29 19:20:01 +0530

    Added a loop problem using while loop

---
+++ 27_for_loop_problem.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int num,i=0;
+    printf("Enter a number please\n");
+    scanf("%d",&num);
+     
+     for(i; i<=num; i++)
+     {
+         printf("%d\n", i);
+
+     }
+return 0;
+}
