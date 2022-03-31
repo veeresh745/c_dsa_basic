3dff707b0fad2fd0ce5b478fa9338b58f0bd871c
veeresh soni <veereshsoni913@gmail.com>
2022-03-31 11:01:43 +0530

    game of odd even

---
+++ 32_problem.c
@@ -0,0 +1,13 @@
+/*keep taking numbers from user until user enters an
+odd number*/
+
+#include <stdio.h>
+int main()
+{
+    int num;
+    do{
+        printf("Enter number please\n"); 
+        scanf("%d", &num);
+    }while(num%2==0);                  //checks whether enterd number is even
+    
+}
