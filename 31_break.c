c0b2bf79cf13ebf6d1badc4301846353f86db81b
veeresh soni <veereshsoni913@gmail.com>
2022-03-31 10:52:19 +0530

    Modification on comments

--- 31_break.c
+++ 31_break.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    for(int i=1; i<=5; i++)
+    for(int i=1; i<=5; i++)           //i is supposed to reach the value of 5 , but we will stop at 3
     {
         printf("values in the loop : %d\n", i);
          if(i==3){                     //loop will stop when value of i reaches to 3
