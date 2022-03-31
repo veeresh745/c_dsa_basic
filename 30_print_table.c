61918eb7d6841b87f69cda7af38439b36a72afcc
veeresh soni <veereshsoni913@gmail.com>
2022-03-31 10:39:48 +0530

    Some modifications

--- 30_print_table.c
+++ 30_print_table.c
@@ -1,14 +1,13 @@
-//print the table of a number input by the user.
-#include<stdio.h>
+// print the table of a number input by the user.
+#include <stdio.h>
 int main()
 {
     int num;
     printf("Enter any number whose table you want: ");
-    scanf("%d",&num);
-    for(int i=1; i<=10; i++)
+    scanf("%d", &num);
+    for (int i = 1; i <= 10; i++)
     {
-        printf(" %d\n", num*i);   //num multiply by i as i increases acc. to table
-        
+        printf(" %d\n", num * i); // num multiply by i as i increases acc. to table
     }
     return 0;
 }
