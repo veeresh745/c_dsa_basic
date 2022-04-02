0aafd415614d88c42851634c6d8040b7c70fbe63
veeresh soni <veereshsoni913@gmail.com>
2022-04-02 11:26:18 +0530

    Modifications on mutiple program

--- 33_multiple_of_7.c
+++ 33_multiple_of_7.c
@@ -1,12 +1,17 @@
 /* keep taking numbers as input from user until user
 enters a number which is multiple of 7.*/
-
+//break will take you out if you used nested loop 
 #include<stdio.h>
 int main(){
     int num;
     do{
         printf("Enter number please\n");
         scanf("%d",&num);
-    }while(num%7==0);
+
+        if(num%7 == 0){
+            break;
+        }
+    }while(1);
+    printf("Yup you just entered a number that is multiple of 7");
     return 0;
 }
