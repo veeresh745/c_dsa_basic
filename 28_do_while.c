3cad03554871a434d5c0b595e346ac91edeb8293
veeresh soni <veereshsoni913@gmail.com>
2022-04-02 11:29:05 +0530

    updation and addition of problem

--- 28_do_while.c
+++ 28_do_while.c
@@ -1,21 +1,24 @@
-//do while runs at least one time
-#include<stdio.h>
-int main(){
-    int i=1;
-    int j=5;
-printf("increment here\n");
-do{
-    
-    printf("%d\n", i);
-    i++;                //increment
-}while(i<=5);
-printf("Decrement:  \n");
+// do while runs at least one time
+#include <stdio.h>
+int main()
+{
+    int i = 1;
+    int j = 5;
+    printf("increment here\n");
+    do
+    {
 
-do{
-    
-   printf("%d\n", j);
-   i--;
-}while(i>=1);
+        printf("%d\n", i);
+        i++; // increment
+    } while (i <= 5);
+    printf("Decrement:  \n");
 
-return 0;
+    do
+    {
+
+        printf("%d\n", j);
+        i--;
+    } while (i >= 1);
+
+    return 0;
 }
---
+++ 34_continue.c
@@ -0,0 +1,12 @@
+//use of continue 
+#include<stdio.h>
+int main()
+{
+    for(int i=1; i<=5; i++){
+        if(i==3){//skip
+            continue;
+        }
+        printf("%d\n", i);
+    }
+    return 0;
+}
