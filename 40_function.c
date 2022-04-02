a47ab48ad4cc6c88f1c037874156227994356d56
veeresh soni <veereshsoni913@gmail.com>
2022-04-02 22:11:09 +0530

    Adding function

--- 40_function.c
+++ 40_function.c
@@ -1,1 +1,15 @@
-//function important for dsa and real life problems when implemented using recurssion.
+// function important for dsa and real life problems when implemented using recurssion.
+//* function: a block of code that performs particular task
+#include <stdio.h>
+void printhello(); // declaration or prototype
+
+int main()
+{
+    printhello(); // function call
+    printhello();
+}
+
+void printhello() // function definition
+{
+    printf("Hello!\n");
+}
