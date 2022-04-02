21bc63f4c2d54c68ec059700b72349c8e03ff7b7
veeresh soni <veereshsoni913@gmail.com>
2022-04-02 22:17:28 +0530

    Adding problem

---
+++ 41_function_problem.c
@@ -0,0 +1,20 @@
+/*write two function -one to print hello and second to 
+print good bye*/
+#include<stdio.h>
+void printHello();
+void printGoodbye();
+
+int main(){
+   printHello();
+   printGoodbye();
+
+    return 0;
+}
+
+void printHello(){
+    printf("Hello\n");
+}
+
+void printGoodbye(){
+    printf("Good Bye\n");
+}
