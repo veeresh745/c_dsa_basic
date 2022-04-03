811b3e7dcd3a1c0e586302fc6a2956723e46f122
veeresh soni <veereshsoni913@gmail.com>
2022-04-03 20:59:35 +0530

    Adding a function para and ar

---
+++ 45_arg_para.c
@@ -0,0 +1,16 @@
+/*Argument:
+1. values that are passed in function call.
+2.  used to send value
+3. actual parameter
+
+Parameter:
+1. vales in function declaration and definition
+2.  used to receive value
+3.  formal parameters
+
+Function:- 
+a. Function can only return one value at a time
+b. changes to parameters in function don't change the values in calling function.
+  because a copy of argument is passes to the function.
+  */
+ 
