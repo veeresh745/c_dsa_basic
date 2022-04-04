af17d9d16c57a88bd642b3d7ba17ef89514aff4c
veeresh soni <veereshsoni913@gmail.com>
2022-04-04 12:17:40 +0530

    Modifications on recursion program

--- 49_n_natural_sum.c
+++ 49_n_natural_sum.c
@@ -1,20 +1,23 @@
-//sum of n natural number using recursion.
+// sum of n natural number using recursion.
 //*1+2+3+4+5----------n-1 + n = sum(n-1) +n
 
-#include<stdio.h>
+#include <stdio.h>
 int sum(int n);
-int main(){
+int main()
+{
     printf("sum is : %d", sum(5));
 
     return 0;
 }
 
-//recursive function
-int sum(int n){
-    if(n==1){
+// recursive function
+int sum(int n)
+{
+    if (n == 1)
+    {
         return 1;
     }
-    int sumnm1 = sum(n-1); //sum of 1 to n
-    int sumn = sumnm1 +n;
+    int sumnm1 = sum(n - 1); // sum from 1 to n
+    int sumn = sumnm1 + n;
     return sumn;
 }
