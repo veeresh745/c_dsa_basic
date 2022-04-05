7fff803e2782434326c69d59ac3c7caefbed30d6
veeresh soni <veereshsoni913@gmail.com>
2022-04-05 23:09:09 +0530

    adding temperature and pow function

---
+++ 67_pow.c
@@ -0,0 +1,22 @@
+// Make your own pow function.
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int power(int x, int y);
+    int x, y;
+    printf("Enter two number considering x to the power y\n");
+    scanf("%d %d", &x, &y);
+
+    printf("%d to the power %d is %d\n", x, y, power(x, y));
+}
+
+int power(int a, int b)
+{
+    int powe=1;
+    for(int i=1; i<= b; ++i){
+        powe= powe *a;
+    }
+    
+    return powe;
+}
