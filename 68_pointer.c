8235961e068c62bc227e02d8d9d0109ac8742659
veeresh soni <veereshsoni913@gmail.com>
2022-04-06 10:41:40 +0530

    Adding pointer program basic

--- 68_pointer.c
+++ 68_pointer.c
@@ -1,18 +1,11 @@
+// pointer: a variable that stores the memory address of another variable.
 #include <stdio.h>
-#include<math.h>
 int main()
 {
-    int power(int x, int y);
-    int x, y;
-    printf("Enter two number considering x to the power y\n");
-    scanf("%d %d", &x, &y);
+    int age = 22;
+    int *ptr = &age;
+    int _age = *ptr;
 
-    printf("%d to the power %d is %d\n", x, y, power(x, y));
-}
-
-int power(int a, int b)
-{
-    int result;
-    result = pow(a,b);
-    return result;
+    printf("%d", _age);
+    return 0;
 }
