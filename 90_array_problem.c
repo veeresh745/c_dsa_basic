d9ca5ae4da0c12a13e669a88014f60b2ffc4928d
veeresh soni <veereshsoni913@gmail.com>
2022-04-08 11:39:59 +0530

    Adding array problem

--- 90_array_problem.c
+++ 90_array_problem.c
@@ -1,18 +1,24 @@
 /* write a function to count the number of odd
 numbers in an array*/
 #include <stdio.h>
-int oddCount(int num[5]);
+int oddCount(int num[], int n);
 int main()
 {
-    int num[5];
-    printf("Enter any 5 numbers to check :\n");
-    for (int i = 0; i <= 4; i++)
-    {
-    }
-
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    printf("%d", oddCount(arr, 6));
     return 0;
 }
 
-int oddCount(int num[5])
+int oddCount(int num[], int n)
 {
+
+    int count = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        if (num[i] % 2 != 0)
+        { // odd
+            count++;
+        }
+    }
+    return count;
 }
