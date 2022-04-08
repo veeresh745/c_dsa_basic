537d3989b33571554f5081b9030b43c2587f8fbd
veeresh soni <veereshsoni913@gmail.com>
2022-04-09 00:21:38 +0530

    Adding reverse array program

---
+++ 93_array_reverse.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int reverse(int arr[], int n);
+void printArray(int arr[], int n);
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    reverse(arr, 5);
+    printArray(arr, 5);
+
+    return 0;
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+    printf("\n");
+}
+
+int reverse(int arr[], int n)
+{
+    for (int i = 0; i < n / 2; i++)
+    {
+        int firstval = arr[i];
+        int secondval = arr[n - i - 1];
+        arr[i] = secondval;
+        arr[n - i - 1] = firstval;
+    }
+}
