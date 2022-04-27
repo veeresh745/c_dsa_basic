d1aa13bc989314e56a72595bfceb72a3d3076ae7
veeresh soni <veereshsoni913@gmail.com>
2022-04-27 06:55:58 +0530

    Adding rooms for strings

---
+++ 96_string.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    //charater defined in single quotes ''
+    //strings defines in double quotes  ""
+    char name[] = {'s', 'h', 'r', 'd', 'h', 'a', '\0'};  //character declaration
+    char name[] = "sharadha";                            //string declaration
+
+}
