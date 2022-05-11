a4f06061a91f471c5de6fd8ffd49e9f941549266
veeresh soni <veereshsoni913@gmail.com>
2022-05-11 23:47:48 +0530

    Adding standard library function]

--- 102_standar_library.c
+++ 102_standar_library.c
@@ -1,1 +1,15 @@
 //strlen(str):- counts number of characters excluding '\0'
+#include<stdio.h>
+void printString(char arr[]);
+int main(){
+    char name[50];
+    scanf("%s", name);          //%s automatically adds '\0'
+    printf("your name is %s", name);
+    return 0;
+}
+void printString(char arr[]){
+    for(int i = 0; arr[i] != '\0'; i++){
+        printf("%c", arr[i]);
+    }
+    printf("\n");
+}
