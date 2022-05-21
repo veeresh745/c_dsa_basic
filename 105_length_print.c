dc095450178b22ff8003c45fa37dbb479e1eee46
veeresh soni <veereshsoni913@gmail.com>
2022-05-21 23:25:32 +0530

    Adding final corrected program

--- 105_length_print.c
+++ 105_length_print.c
@@ -1,5 +1,30 @@
-//make a program that inputs user's name and print its length
-#include<stdio.h>
-int main(){
-    
+// make a program that inputs user's name and print its length
+#include <stdio.h>
+void printString(char arr[]);
+int countLength(char arr[]);
+int main()
+{
+    char name[100];
+    fgets(name, 100, stdin);
+    printf("length is: %d", countLength(name));
+
+    return 0;
+}
+int countLength(char arr[])
+{
+    int count = 0;
+    for (int i = 0; arr[i] != '\0'; i++)
+    {
+        count++;
+    }
+    return count-1;
+}
+
+void printString(char arr[])
+{
+    for (int i = 0; arr[i] != '\0'; i++)
+    {
+        printf("%c", arr[i]);
+    }
+    printf("\n");
 }
