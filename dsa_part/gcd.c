ac8f72ad6f5c995531daa58607d00c4d7a8897eb
veeresh soni <veereshsoni913@gmail.com>
2022-06-18 23:48:17 +0530

    Adding programs

---
+++ dsa_part/gcd.c
@@ -0,0 +1,17 @@
+//program to find greatest common divisor
+#include<stdio.h>
+int gcd(int x, int y);
+int main(){
+    int x,y;
+    printf("Enter numbers");
+    scanf("%d %d", &x,&y);
+
+    int res=gcd(x,y);
+    printf("%d GCD:", res);
+
+}
+
+int gcd(int m, int n){
+    
+
+}
