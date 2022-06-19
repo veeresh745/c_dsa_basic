6526cfa8fbfa20588ef766273b680c9a99fb0182
veeresh soni <veereshsoni913@gmail.com>
2022-06-19 22:50:40 +0530

    Adding programs

--- dsa_part/11_lcm_efficient.c
+++ dsa_part/11_lcm_efficient.c
@@ -1,1 +1,29 @@
-//efficient lcm program
+// efficient lcm program
+#include <stdio.h>
+int lcm(int x, int y);
+int main()
+{
+    int x, y;
+    printf("Enter numbers");
+    scanf("%d %d", &x, &y);
+
+    int result = lcm(x, y);
+    printf("LCM: %d", result);
+}
+int gcd(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    else
+    {
+        return gcd(b, a % b);
+    }
+}
+int lcm(int a, int b)
+{
+    return (a*b)/gcd(a,b);
+}
+
+//time complexity: O(log(min(a,b)))
