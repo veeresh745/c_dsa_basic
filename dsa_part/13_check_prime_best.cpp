f0509e97401d081c81d8e494a9efc5e9075c8180
veeresh soni <veereshsoni913@gmail.com>
2022-06-20 20:29:42 +0530

    Adding programs

---
+++ dsa_part/13_check_prime_best.cpp
@@ -0,0 +1,38 @@
+//Checking if a given number is prime or not
+#include <bits/stdc++.h>
+using namespace std;
+bool isPrime(int n);
+
+int main()
+{
+    int n;
+    cout << "Enter number to check";
+    cin >> n;
+
+    bool res = isPrime(n);
+
+    if (res == 1)
+    {
+        cout << "Prime number";
+    }
+    else
+    {
+        cout << "Not a prime number";
+    }
+}
+
+bool isPrime(int num)
+{
+    if (num == 1)
+    {
+        return false;
+    }
+    for (int i = 2; i*i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ dsa_part/try.c
@@ -0,0 +1,4 @@
+/*Find the mininmum number of squares that sum to a given number.
+Given a positive integer n, find the minimum number of squares that sums to n.
+*/
+int minimumSquare()
