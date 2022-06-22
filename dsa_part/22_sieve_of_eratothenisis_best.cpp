7773246513307e9989f0507053dec8739eb4ae03
veeresh soni <veereshsoni913@gmail.com>
2022-06-22 23:37:12 +0530

    Adding comments for program

---
+++ dsa_part/22_sieve_of_eratothenisis_best.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+int sieve(int input);
+int main()
+{
+    int input;
+    cout << "Enter number ";
+    cin >> input;
+
+    int res = sieve(input);
+}
+
+int isPrime(int number)
+{
+    if (number == 1)
+    {
+        return 0;
+    }
+    for (int i = 2; i < number; i++)
+    {
+        if (number % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int sieve(int n)
+{
+    vector<bool> isPrime(n + 1, true);
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (isPrime[i])
+        {
+            for (int j = 2 * i; j <= n; j = j + i)
+            {
+                isPrime[j] = false;
+            }
+        }
+        for (int i = 2; i <= n; i++)
+        {
+            if (isPrime[i])
+            {
+                cout << i << " ";
+            }
+        }
+    }
+}
