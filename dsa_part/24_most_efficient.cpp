7c8f07fab047f81509c72933e0b22df956abcdce
veeresh soni <veereshsoni913@gmail.com>
2022-06-23 22:18:30 +0530

    sieve of eratothenisis most efficient

--- dsa_part/24_most_efficient.cpp
+++ dsa_part/24_most_efficient.cpp
@@ -26,3 +26,18 @@ int isPrime(int number)
     return 1;
 }
 
+int sieve(int n)
+{
+    vector<bool> isPrime(n + 1, true);
+    for (int i = 2; i <= n; i++)
+    {
+        if (isPrime[i])
+        {
+            cout << i << " ";
+            for (int j = i * i; j <= n; j = j + i)
+            {
+                isPrime[j] = false;
+            }
+        }
+    }
+}
